e775cda2007069080df7b3ff901a95c9d8beaab5
Mattia Sinisi <mattia@snisni.it>
2021-05-11 22:33:44 +0200

    Get default parameters from separate file

---
+++ defaults.js
@@ -0,0 +1,5 @@
+module.exports = {
+	title: "Built with love",
+	borderRadius: 20,
+	backgroundImage: "https://images.pexels.com/photos/1183021/pexels-photo-1183021.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+}
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const generator = require('node-html-to-image');
 const fs = require('fs');
 const server = require('express')();
 
+const defaults = require('./defaults');
+
 
 server.get('/', async function (req, res) {
 	let template = fs.readFileSync("template.html").toString();
@@ -9,11 +11,7 @@ server.get('/', async function (req, res) {
 	const image = await generator({
 		html: template,
 		transparent: true,
-		content: Object.assign({
-			title: "Built with love",
-			borderRadius: 20,
-			backgroundImage: "https://images.pexels.com/photos/1183021/pexels-photo-1183021.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-		}, req.query)
+		content: Object.assign(defaults, req.query)
 	});
 
 	res.writeHead(200, { 'Content-Type': 'image/png' });
