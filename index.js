ee05a66cee82817a67c687926e26247746200f76
Mattia Sinisi <mattia@snisni.it>
2021-05-11 21:53:29 +0200

    Using HTML file as template

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const generator = require('node-html-to-image');
+const fs = require('fs');
+
+let template = fs.readFileSync("template.html").toString();
 
 generator({
 	output: './image.png',
-	html: '<html><body>Hello world!</body></html>'
+	html: template
   })
 .then(() => console.log('The image was created successfully!'))
   
