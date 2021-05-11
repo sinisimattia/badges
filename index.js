06fa405a22c86754328e43bb2b512cf31dc31f2f
Mattia Sinisi <mattia@snisni.it>
2021-05-11 22:28:13 +0200

    Custom border radius

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ server.get('/', async function (req, res) {
 	let template = fs.readFileSync("template.html").toString();
 
 	const image = await generator({
-		output: './image.png',
 		html: template,
+		transparent: true,
 		content: {
+			title: "Built with love",
+			borderRadius: 20,
 			backgroundImage: "https://images.pexels.com/photos/1183021/pexels-photo-1183021.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
 		}
 	});
