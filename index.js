c669670a8a462a3a3fa194502bcea4c052f68376
Mattia Sinisi <mattia@snisni.it>
2021-05-11 22:08:27 +0200

    Serve image via HTTP and interpolate data in template

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const express = require('express');
 const generator = require('node-html-to-image');
 const fs = require('fs');
+const server = require('express')();
 
-let template = fs.readFileSync("template.html").toString();
 
-generator({
-	output: './image.png',
-	html: template
-  })
-.then(() => console.log('The image was created successfully!'))
+server.get('/', async function (req, res) {
+	let template = fs.readFileSync("template.html").toString();
+
+	const image = await generator({
+		output: './image.png',
+		html: template,
+		content: {
+			backgroundImage: "https://images.pexels.com/photos/1183021/pexels-photo-1183021.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+		}
+	});
+
+	res.writeHead(200, { 'Content-Type': 'image/png' });
+ 	res.end(image, 'binary');
+
+})
+
+server.listen(process.env.PORT || 80, () => {
+	console.log("Server started.")
+});
   
